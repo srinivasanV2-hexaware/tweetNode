b272502b6a5b78930d5125db3e298e829d5359fe
Ashwin Ganesh <41741@hexaware.com>
2018-07-12 17:08:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const Twitter = require('twitter');
+const config = {
+    consumer_key : process.env.consumer_key,
+    consumer_secret : process.env.consumer_secret,
+    access_token : process.env.access_token,
+    access_token_secret : process.env.access_token_secret
+};
+
+const tweet =  new Twitter(config);
+
+const params = {
+    q: '#nodejs',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+tweet.get('search/tweets', params, function(err, data, response) {
+    if(!err){
+      console.log(data);
+    } else {
+      console.log(err);
+    }
+})
+  
+
+
