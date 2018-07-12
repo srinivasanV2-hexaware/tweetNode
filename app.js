2d2e2be11ce9152634b90798f9eb922db5669e7a
Ashwin Ganesh <41741@hexaware.com>
2018-07-12 18:32:59 +0530

    packageUpdate

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Twitter = require('twitter');
 const config = {
     consumer_key : process.env.consumer_key,
     consumer_secret : process.env.consumer_secret,
-    access_token : process.env.access_token,
+    access_token_key : process.env.access_token,
     access_token_secret : process.env.access_token_secret
 };
 
